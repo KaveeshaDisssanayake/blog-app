caec84e0fb7da1c69dce8c93da1108d65ff95a88
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-06-29 15:02:24 +0530

    blog post page

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -15,7 +15,7 @@ const BlogList = () => {
             </div>
             <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
                 {blog_data.filter((item) => menu === "All" ? true : item.category === menu).map((item) => (
-                    <BlogItem key={item.id} image={item.image} title={item.title} description={item.description} category={item.category} />
+                    <BlogItem key={item.id} id={item.id} image={item.image} title={item.title} description={item.description} category={item.category} />
                 ))}
             </div>
         </div>
