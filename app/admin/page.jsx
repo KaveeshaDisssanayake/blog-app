2d055dd779eb3292f9708639ff906c8da31d0a3e
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-07-03 17:28:01 +0530

    finalize

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div class="flex">
+        <div class="w-1/4 bg-white p-4">
+            <div class="text-bold text-center font-[100px]  mb-40">Welcome To Admin Panel</div>
+            
+        </div>
+        </div>
   )
 }
 
--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -43,16 +43,12 @@ const page = ({ params }) => {
                 </div>
                 <div className="mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10">
                     <Image className="border-4 border-white" src={data.image} width={1280} height={720} alt="" />
-                    <h1 className="my-8 text-[26px] font-semibold">Introduction:</h1>
-                    <p>{data.description}</p>
-                    <h3 className="my-5 text-[18px] font-semibold">Step 1: Self Reflection and Goals</h3>
-                    <p className="my-3">Unlock your potential and immortalize special memories by honing your skills and capturing moments.</p>
-                    <h3 className="my-5 text-[18px] font-semibold">Step 2: Self Reflection and Goals</h3>
-                    <p className="my-3">Unlock your potential and immortalize special memories by honing your skills and capturing moments.</p>
-                    <h3 className="my-5 text-[18px] font-semibold">Step 3: Self Reflection and Goals</h3>
-                    <p className="my-3">Unlock your potential and immortalize special memories by honing your skills and capturing moments.</p>
-                    <h3 className="my-5 text-[18px] font-semibold">Conclusion: </h3>
-                    <p className="my-3">Unlock your potential and immortalize special memories by honing your skills and capturing moments.</p>
+
+                   <div className="blog-content" dangerouslySetInnerHTML={{__html:data.description}}>
+
+                   </div>
+                    
+                    
                 <div className="my-24 ">
                    <p className="text-black font-semibold my-4">Share this article on Social Media</p>
                    <div className="flex">
